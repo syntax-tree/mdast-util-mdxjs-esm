f01210d52042bff8fc4f1fa1a3740077a6b162fb
Titus Wormer <tituswormer@gmail.com>
2022-05-22 13:03:29 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -73,35 +73,30 @@ test('markdown -> mdast', (t) => {
               type: 'Program',
               start: 0,
               end: 35,
-              loc: {start: {line: 1, column: 0}, end: {line: 2, column: 17}},
               body: [
                 {
                   type: 'ImportDeclaration',
                   start: 0,
                   end: 17,
-                  loc: {
-                    start: {line: 1, column: 0},
-                    end: {line: 1, column: 17}
-                  },
                   specifiers: [
                     {
                       type: 'ImportDefaultSpecifier',
                       start: 7,
                       end: 8,
-                      loc: {
-                        start: {line: 1, column: 7},
-                        end: {line: 1, column: 8}
-                      },
                       local: {
                         type: 'Identifier',
                         start: 7,
                         end: 8,
-                        loc: {
-                          start: {line: 1, column: 7},
-                          end: {line: 1, column: 8}
-                        },
                         name: 'a',
+                        loc: {
+                          start: {line: 1, column: 7, offset: 7},
+                          end: {line: 1, column: 8, offset: 8}
+                        },
                         range: [7, 8]
+                      },
+                      loc: {
+                        start: {line: 1, column: 7, offset: 7},
+                        end: {line: 1, column: 8, offset: 8}
                       },
                       range: [7, 8]
                     }
@@ -110,13 +105,17 @@ test('markdown -> mdast', (t) => {
                     type: 'Literal',
                     start: 14,
                     end: 17,
-                    loc: {
-                      start: {line: 1, column: 14},
-                      end: {line: 1, column: 17}
-                    },
                     value: 'b',
                     raw: '"b"',
+                    loc: {
+                      start: {line: 1, column: 14, offset: 14},
+                      end: {line: 1, column: 17, offset: 17}
+                    },
                     range: [14, 17]
+                  },
+                  loc: {
+                    start: {line: 1, column: 0, offset: 0},
+                    end: {line: 1, column: 17, offset: 17}
                   },
                   range: [0, 17]
                 },
@@ -124,63 +123,67 @@ test('markdown -> mdast', (t) => {
                   type: 'ExportNamedDeclaration',
                   start: 18,
                   end: 35,
-                  loc: {
-                    start: {line: 2, column: 0},
-                    end: {line: 2, column: 17}
-                  },
                   declaration: {
                     type: 'VariableDeclaration',
                     start: 25,
                     end: 35,
-                    loc: {
-                      start: {line: 2, column: 7},
-                      end: {line: 2, column: 17}
-                    },
                     declarations: [
                       {
                         type: 'VariableDeclarator',
                         start: 29,
                         end: 35,
-                        loc: {
-                          start: {line: 2, column: 11},
-                          end: {line: 2, column: 17}
-                        },
                         id: {
                           type: 'Identifier',
                           start: 29,
                           end: 30,
-                          loc: {
-                            start: {line: 2, column: 11},
-                            end: {line: 2, column: 12}
-                          },
                           name: 'c',
+                          loc: {
+                            start: {line: 2, column: 11, offset: 29},
+                            end: {line: 2, column: 12, offset: 30}
+                          },
                           range: [29, 30]
                         },
                         init: {
                           type: 'Literal',
                           start: 33,
                           end: 35,
-                          loc: {
-                            start: {line: 2, column: 15},
-                            end: {line: 2, column: 17}
-                          },
                           value: '',
                           raw: '""',
+                          loc: {
+                            start: {line: 2, column: 15, offset: 33},
+                            end: {line: 2, column: 17, offset: 35}
+                          },
                           range: [33, 35]
+                        },
+                        loc: {
+                          start: {line: 2, column: 11, offset: 29},
+                          end: {line: 2, column: 17, offset: 35}
                         },
                         range: [29, 35]
                       }
                     ],
                     kind: 'var',
+                    loc: {
+                      start: {line: 2, column: 7, offset: 25},
+                      end: {line: 2, column: 17, offset: 35}
+                    },
                     range: [25, 35]
                   },
                   specifiers: [],
                   source: null,
+                  loc: {
+                    start: {line: 2, column: 0, offset: 18},
+                    end: {line: 2, column: 17, offset: 35}
+                  },
                   range: [18, 35]
                 }
               ],
               sourceType: 'module',
               comments: [],
+              loc: {
+                start: {line: 1, column: 0, offset: 0},
+                end: {line: 2, column: 17, offset: 35}
+              },
               range: [0, 35]
             }
           }
