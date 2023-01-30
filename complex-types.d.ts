3f980d104de70d4c07706246ba38fd0cb71d7325
Titus Wormer <tituswormer@gmail.com>
2023-01-30 15:28:38 +0100

    Add comments to node types

--- complex-types.d.ts
+++ complex-types.d.ts
@@ -1,26 +1,4 @@
-import type {Literal} from 'mdast'
-import type {Program} from 'estree-jsx'
+// To do: next major: remove this file.
+export type {MdxjsEsm} from './index.js'
 
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-export interface MdxjsEsm extends Literal {
-  type: 'mdxjsEsm'
-  data?: {estree?: Program}
-}
-
-declare module 'mdast' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface BlockContentMap {
-    mdxjsEsm: MdxjsEsm
-  }
-}
-
-declare module 'hast' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface RootContentMap {
-    mdxjsEsm: MdxjsEsm
-  }
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface ElementContentMap {
-    mdxjsEsm: MdxjsEsm
-  }
-}
+/// <reference types="./index.js" />
---
+++ index.d.ts
@@ -0,0 +1,55 @@
+import type {Literal} from 'mdast'
+import type {Program} from 'estree-jsx'
+
+export {mdxjsEsmFromMarkdown, mdxjsEsmToMarkdown} from './lib/index.js'
+
+/**
+ * MDX ESM (import/export) node.
+ */
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export interface MdxjsEsm extends Literal {
+  /**
+   * Node type.
+   */
+  type: 'mdxjsEsm'
+
+  /**
+   * Data.
+   */
+  data?: {
+    /**
+     * Program node from estree.
+     */
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    estree?: Program | null | undefined
+  }
+}
+
+// Add nodes to content.
+declare module 'mdast' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface BlockContentMap {
+    /**
+     * MDX ESM.
+     */
+    mdxjsEsm: MdxjsEsm
+  }
+}
+
+declare module 'hast' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface RootContentMap {
+    /**
+     * MDX ESM.
+     */
+    mdxjsEsm: MdxjsEsm
+  }
+
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ElementContentMap {
+    /**
+     * MDX ESM.
+     */
+    mdxjsEsm: MdxjsEsm
+  }
+}
